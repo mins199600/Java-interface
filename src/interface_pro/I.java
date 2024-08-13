a46386ebf95998654b143c01631299423d524ca4
sumin <mins19960091@gmail.com>
2024-08-13 13:03:21 +0900

    자바 인터페이스 공부 및 깃허브 공부

---
+++ src/interface_pro/I.java
@@ -0,0 +1,6 @@
+package interface_pro;
+
+public interface I {
+	public void z();
+
+}
---
+++ src/interface_pro/interfacePractice.java
@@ -0,0 +1,7 @@
+package interface_pro;
+
+public class interfacePractice implements I{
+	//interfacePractice라는 클래스가 I 클래스를 구현한다
+	public void z() {}
+	//특정한 클래스에 특정한 인터페이스를 구현한다
+}
